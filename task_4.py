05377e3bb6f32f1fa2dadb94d1acedf326865b91
Zaurbek Dreev <zaurbekdreev@gmail.com>
2025-04-29 09:23:12 +0300

    add task_4

---
+++ task_4.py
@@ -0,0 +1,27 @@
+class EmployeeSalary:
+
+    hourly_payment = 400
+
+    def __init__(self, name, hours, rest_days, email):
+        self.name = name
+        self.hours = hours
+        self.rest_days = rest_days
+        self.email = email
+
+    @classmethod
+    def get_hours(cls, name, rest_days, email):
+        hours = (7 - rest_days) * 8
+        return cls(name, hours, rest_days, email)
+    
+    @classmethod
+    def get_email(cls, name, hours, rest_days):
+        email = f'{name}@email.com'
+        return cls(name, hours, rest_days, email)
+    
+    @classmethod
+    def set_hourly_payment(cls, new_hourly_payment):
+        cls.hourly_payment = new_hourly_payment
+
+    # в ТЗ не указано, что необходимо возвращать значение, но я люблю рисковать
+    def salary(self):
+        return self.hours * self.hourly_payment
